9dbfd2311b8cf16ea95d5e3555fbf1fab14be69f
Dhananjaypathak27 <43525854+Dhananjaypathak27@users.noreply.github.com>
2020-04-16 15:05:30 +0530

    bugs fixed and ui improved

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ $(function(){
       alert("enter data");
     }else{
       value=$("#userinput").val();
-      value2=$("ul").prepend("<li id='listItem'>"+ value + "&nbsp "  +"<button class='btn btn-dark p-0' id='removeMe' onClick='removeThis(this)' >  &times;</button>"+"</li>");
+      value2=$("ul").prepend("<li id='listItem'>"+ value + "&nbsp "  +"<button class='btn btn-dark' id='removeMe' onClick='removeThis(this)' >  &times;</button>"+"</li>");
       // value2.attr("id","list");
     }
     $('#userinput').val("");
